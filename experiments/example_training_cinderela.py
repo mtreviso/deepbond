fb9ef04171803b7f5513626abacbce1f2a56d938
Marcos Treviso <marcosvtreviso@gmail.com>
2019-09-13 17:14:56 -0300

    Update example

--- experiments/example_training_cinderela.py
+++ experiments/example_training_cinderela.py
@@ -5,61 +5,54 @@ import os
 text = 'Há livros escritos para evitar espaços vazios na estante .'
 
 args = {
-  #'train_path': 'data/transcriptions/ss/Controle/',
-  #'dev_path': 'data/transcriptions/ss/CCL-A/',
-  #'test_path': 'data/transcriptions/ss/DA-Leve/',
+    'model': 'rcnn',
+    'rnn_type': 'lstm',
+    'bidirectional': True,
+    'train_batch_size': 4,
+    'dev_batch_size': 4,
+    'optimizer': 'rmsprop',
+    'final_report': True,
+    'epochs': 1,
+    'hidden_size': [100],
 
-  'model': 'rcnn',
-  'rnn_type': 'lstm',
-  'bidirectional': True,
-  'train_batch_size': 4,
-  'dev_batch_size': 4,
-  'optimizer': 'rmsprop',
-  'final_report': True,
-  'epochs': 1,
-  'hidden_size': [100],
+    'punctuations': '.?!',
+    'loss_weights': 'balanced',
+    # 'embeddings_format': 'word2vec',
+    # 'embeddings_path': 'data/embeddings/word2vec/pt_word2vec_sg_600.kv.emb',
 
-  'punctuations': '.?!',
-  'loss_weights': 'balanced',
-  # 'embeddings_format': 'word2vec',
-  # 'embeddings_path': 'data/embeddings/word2vec/pt_word2vec_sg_600.kv.emb',
+    'early_stopping_patience': 5,
+    'restore_best_model': True,  #
 
-  'early_stopping_patience': 5,
-  'restore_best_model': True,  #
+    'save_best_only': True,  # only save the best model
 
-  'save_best_only': True,  # only save the best model
-
-  'output_dir': 'runs/testing-cinderela-lib-mode/',  # keep log information
-  'save': 'saved-models/testing-cinderela-lib-mode/',  # save models
-  'tensorboard': True,
+    'output_dir': 'runs/testing-cinderela-lib-mode/',  # keep log information
+    'save': 'saved-models/testing-cinderela-lib-mode/',  # save models
+    'tensorboard': True,
 
 }
 
+folds_dir = 'data/transcriptions/folds/CCL-A/'
 
+for fold in os.listdir(folds_dir):
+    print("Fold Number:", fold)
+    train_path = os.path.join(folds_dir, fold, 'train')
+    test_path = os.path.join(folds_dir, fold, 'test')
 
-
-folds_dir = 'data/transcriptions/folds'
-
-for typ in os.listdir(folds_dir):
-  print('Dataset:',typ)
-  for fold in os.listdir(os.path.join(folds_dir,typ)):
-    print("Fold Number:",fold)
-    train_path = os.path.join(folds_dir,typ,fold,'train')
-    test_path = os.path.join(folds_dir,typ,fold,'test')
     tagger = Tagger()
-    tagger.train(train_path=train_path,test_path=test_path ,dropout=0.2, **args)
+    tagger.train(train_path=train_path,
+                 test_path=test_path,
+                 dropout=0.2,
+                 **args)
+
     # alternatively, you can pass them like this:
     # tagger.train(train_path='path/to/', model='rcnn', epochs=2, use_caps=True)
-    classes = tagger.predict_classes(text)
-    probas = tagger.predict_probas(text)
-    tags = tagger.transform_classes_to_tags(classes)
-    tags_probas = tagger.transform_probas_to_tags(probas)
-    print(text)
-    print(classes)
-    print(tags)
-    print(tags_probas)
-    del tagger
 
+    classes = tagger.predict_classes(text)
+    tags = tagger.transform_classes_to_tags(classes)
+
+    print(text)
+    print(tags)
+    del tagger
 
 # loading now!
 tagger = Tagger()
@@ -73,4 +66,3 @@ print(classes)
 print(tags)
 print(tags_probas)
 del tagger
-
